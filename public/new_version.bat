8f2bebaa2498176671a81ef447200306b0c76b1f
General Failure <gf59ru@users.noreply.github.com>
2016-04-08 11:23:39 +0500

    Delete new_version.bat

--- public/new_version.bat
+++
@@ -1,2 +0,0 @@
-copy arcgis_js_api\library\3.12\3.12\init_client.js arcgis_js_api\library\3.12\3.12\init.js
-copy arcgis_js_api\library\3.12\3.12\dojo\dojo_client.js arcgis_js_api\library\3.12\3.12\dojo\dojo.js
